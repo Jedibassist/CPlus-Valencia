2e94c62dc40355011b76f875ab4b4b1b055507fc
Jedibassist <jedibassist@gmail.com>
2013-05-27 21:25:21 -0400

    First Save

---
+++ Loops Assignment 3 J.Gravelin/Loops Assignment 3 J.Gravelin/Loops Assignment 3 J.Gravelin.cpp
@@ -0,0 +1,17 @@
+// Loops Assignment 3 J.Gravelin.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+main(){
+
+	int sum = 0, counter = 1;
+
+	while(counter < 1){
+	sum++;
+	}
+	
+	cout << "The value of sum is " <<
+}
