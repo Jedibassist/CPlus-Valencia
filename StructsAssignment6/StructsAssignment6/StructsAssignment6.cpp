fed2f4d49aabad2d5662d08fba875beb53513fea
Jedibassist <jedibassist@gmail.com>
2013-06-24 15:35:15 -0400

    6/24/2013 Update

--- StructsAssignment6/StructsAssignment6/StructsAssignment6.cpp
+++ StructsAssignment6/StructsAssignment6/StructsAssignment6.cpp
@@ -1,21 +1,31 @@
-// StructsAssignment6.cpp : Defines the entry point for the console application.
-//
+// Jed Gravelin - June 24th, 2013
+// C++ Programming, Valencia College, Summer 2013
 
 #include "stdafx.h"
 using namespace std;
 
+// Struct for a Student
+// Each Student has a VID and a name
 struct student{
 	string name;
 	int VID;
 };
 
+// Struct for a Course
+// Each course has 3 Students and a Name
 struct course{
 	student students[3];
+	string name; // I felt the course needed a name.
 };
 
 void main (){
-	course courses[2];
+	course courses[2]; // Set Array of Courses
 
+	// Set Course Names
+	courses[0].name = "C++ Programming";
+	courses[1].name = "Data Analysis";
+
+	// Create array of Students and Populate
 	student students[3];
 	students[0].name = "Jed";
 	students[0].VID = 12345;
@@ -35,11 +45,12 @@ void main (){
 		}
 	}
 
-	// Print out the students VID's
+	// Print out the courses with enrolled student VID's and names
 	for(i = 0; i < sizeof(courses)/sizeof(*courses); i++){
-		cout << "Course " << i + 1 << " student VID's:" << endl;
+		cout << courses[i].name << " students:" << endl;
 		for(j = 0; j < sizeof(courses[i].students)/sizeof(*courses[i].students); j++){
-			cout << courses[i].students[j].VID << endl;
+			cout << "Student VID :" << courses[i].students[j].VID << "\t"
+				<< "Student Name :" << courses[i].students[j].name << endl;
 		}
 		cout << endl;
 	}
