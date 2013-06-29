afa88551d55000dba868f6cfe92ed64f24724742
Jedibassist <jedibassist@gmail.com>
2013-06-29 16:21:02 -0400

    InputOutput Structs Assign 7

--- StructsAssignment6/StructsAssignment6/StructsAssignment6.cpp
+++ StructsAssignment6/StructsAssignment6/StructsAssignment6.cpp
@@ -2,7 +2,14 @@
 // C++ Programming, Valencia College, Summer 2013
 
 #include "stdafx.h"
+// Other relevant includes are in header file
+
 using namespace std;
+
+
+// Set input and output files
+	ofstream myOutputFile;
+	ifstream myInputFile;
 
 // Struct for a Student
 // Each Student has a VID and a name
@@ -17,6 +24,10 @@ struct course{
 	student students[3];
 	string name; // I felt the course needed a name.
 };
+
+// Function Prototypes
+void printToFile(course coursesToPrint[], int size);
+void printToConsole();
 
 void main (){
 	course courses[2]; // Set Array of Courses
@@ -46,6 +57,7 @@ void main (){
 	}
 
 	// Print out the courses with enrolled student VID's and names
+	/*
 	for(i = 0; i < sizeof(courses)/sizeof(*courses); i++){
 		cout << courses[i].name << " students:" << endl;
 		for(j = 0; j < sizeof(courses[i].students)/sizeof(*courses[i].students); j++){
@@ -54,7 +66,43 @@ void main (){
 		}
 		cout << endl;
 	}
+	*/
+
+	printToFile(courses, sizeof(courses)/sizeof(*courses));
+
+	printToConsole();
 
 	system("pause");
 }
 
+// Pushing the Array data over to a file *pre-formatted
+void printToFile(course coursesToPrint[], int size){
+	myOutputFile.open("C://CourseEnrollment.txt");
+
+	int i, j;
+	for(i = 0; i < size; i++){
+		myOutputFile << coursesToPrint[i].name << " students:" << endl;
+		for(j = 0; j < sizeof(coursesToPrint[i].students)/sizeof(*coursesToPrint[i].students); j++){
+			myOutputFile << "Student VID :" << coursesToPrint[i].students[j].VID << "\t"
+				<< "Student Name :" << coursesToPrint[i].students[j].name << endl;
+		}
+		myOutputFile << endl;
+	}
+	myOutputFile.close();
+}
+
+// Printing the contents of the file to the console
+void printToConsole(){
+	myInputFile.open("C://CourseEnrollment.txt");
+
+	if(myInputFile.is_open()){
+		while(!myInputFile.eof()){
+			string myLine;
+			getline(myInputFile, myLine);
+			cout << myLine << endl;
+		}
+		myInputFile.close();
+	}else{
+		cout << "FAILURE" << endl;
+	}
+}
